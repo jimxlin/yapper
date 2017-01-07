b814d523573eb6ee2621a9b7c930fac66c510035
Jim X. Lin <miljinx@gmail.com>
2017-01-06 18:37:28 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Place.create(name: 'Nasty Callery Pear', address: '22 Bar street, Eek, AK 99578', description: 'Young and brash, very rude to locals.')
+Place.create(name: 'Medium Douglas Fir', address: '11 Foo street, Earth, TX 79031', description: 'About 12 feet tall, exhibits a healthy aura.')
