4e6825f6856aef51904546aaa931a5e2531b02cb
Jim X. Lin <miljinx@gmail.com>
2017-01-12 20:51:45 -0800

    Add image uploading and viewing

--- Gemfile
+++ Gemfile
@@ -63,3 +63,5 @@ gem 'devise'
 gem 'geocoder'
 
 gem 'figaro'
+
+gem 'carrierwave', '~> 1.0'
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#Load CarrierWave after loading ORM
+require 'carrierwave/orm/activerecord'
