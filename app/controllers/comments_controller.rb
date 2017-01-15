6fccb0de73f9f5461e263ec0617edcb05642e3bc
Jim X. Lin <miljinx@gmail.com>
2017-01-14 19:51:27 -0800

    Remove debugger line

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,6 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    console
     @place = Place.find(params[:place_id])
     @place.comments.create(comment_params.merge(user: current_user))
     redirect_to place_path(@place)
